79140f082405e323e9f9f4995bfe58e4e631048a
pmoreno-rodriguez <pmorenorodriguez@protonmail.com>
2025-10-21 17:35:57 +0200

    Removed  empty `onThemeInitialized` function

--- quark-a11y.php
+++ quark-a11y.php
@@ -9,15 +9,9 @@ class QuarkA11y extends Theme
     public static function getSubscribedEvents()
     {
         return [
-            'onThemeInitialized'    => ['onThemeInitialized', 0],
             'onTwigLoader'          => ['onTwigLoader', 0],
             'onTwigInitialized'     => ['onTwigInitialized', 0],
         ];
-    }
-
-    public function onThemeInitialized()
-    {
-
     }
 
     // Add images to twig template paths to allow inclusion of SVG files
